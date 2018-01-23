25f1140e4018c66a135c76ec31c8370a9d412c6c
NickArtemov <alukard1986@gmail.com>
2018-01-23 22:25:15 +0200

    fix

--- src/containers/WidgetPage/WidgetPage.jsx
+++ src/containers/WidgetPage/WidgetPage.jsx
@@ -11,7 +11,7 @@ import {Col, Form, Row} from "reactstrap";
 const b = block('WidgetPage');
 
 const CloseIcon = (props) => (
-    <div onClick={!props.disabled && props.onClick}>
+    <div onClick={!props.disabled ? props.onClick : undefined}>
         <SvgIcon viewBox="0 0 1000 1000" color={props.disabled ? '#c6c6c6' : '#000'}>
             <path
                 d="M638.6,500l322.7-322.7c38.3-38.3,38.3-100.3,0-138.6C923,0.4,861,0.4,822.7,38.7L500,361.4L177.3,38.7C139,0.4,77,0.4,38.7,38.7C0.4,77,0.4,139,38.7,177.3L361.4,500L38.7,822.7C0.4,861,0.4,923,38.7,961.3C57.9,980.4,82.9,990,108,990s50.1-9.6,69.3-28.7L500,638.6l322.7,322.7c19.1,19.1,44.2,28.7,69.3,28.7c25.1,0,50.1-9.6,69.3-28.7c38.3-38.3,38.3-100.3,0-138.6L638.6,500z"/>
