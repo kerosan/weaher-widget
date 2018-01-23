bf757de53eaae07347f641acc0584ed8f38a80f4
NickArtemov <alukard1986@gmail.com>
2018-01-23 20:50:55 +0200

    fix

--- src/containers/WidgetPage/WidgetPage.jsx
+++ src/containers/WidgetPage/WidgetPage.jsx
@@ -1,7 +1,7 @@
 import './WidgetPage.scss';
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import {RaisedButton, SvgIcon, Tab, Tabs} from "material-ui";
+import {SvgIcon, Tab, Tabs} from "material-ui";
 import PropTypes from 'prop-types';
 import {LocationInput} from "../../components/LocationInput/LocationInput";
 import {addLocationAction, deleteLocationAction} from "../../redux/modules/locations";
@@ -11,8 +11,8 @@ import {Col, Form, Row} from "reactstrap";
 const b = block('WidgetPage');
 
 const CloseIcon = (props) => (
-    <div onClick={props.onClick}>
-        <SvgIcon viewBox="0 0 1000 1000">
+    <div onClick={!props.disabled && props.onClick}>
+        <SvgIcon viewBox="0 0 1000 1000" color={props.disabled ? '#c6c6c6' : '#000'}>
             <path
                 d="M638.6,500l322.7-322.7c38.3-38.3,38.3-100.3,0-138.6C923,0.4,861,0.4,822.7,38.7L500,361.4L177.3,38.7C139,0.4,77,0.4,38.7,38.7C0.4,77,0.4,139,38.7,177.3L361.4,500L38.7,822.7C0.4,861,0.4,923,38.7,961.3C57.9,980.4,82.9,990,108,990s50.1-9.6,69.3-28.7L500,638.6l322.7,322.7c19.1,19.1,44.2,28.7,69.3,28.7c25.1,0,50.1-9.6,69.3-28.7c38.3-38.3,38.3-100.3,0-138.6L638.6,500z"/>
         </SvgIcon>
@@ -52,13 +52,6 @@ export class WidgetPage extends Component {
                             onChange={this.props.addLocationAction}
                         />
                     </Col>
-                    {/*<Col xs={2}>*/}
-                        {/*<RaisedButton*/}
-                            {/*label={"Add place"}*/}
-                            {/*style={{width: '100%'}}*/}
-                            {/*onClick={this.onAddPlace}*/}
-                        {/*/>*/}
-                    {/*</Col>*/}
                 </Row>
             </Form>
 
@@ -71,13 +64,17 @@ export class WidgetPage extends Component {
 
                         return <Tab
                             key={key}
-                            icon={<CloseIcon onClick={() => this.closeTab(tab)}/>}
+                            icon={<CloseIcon disabled={key < 2} onClick={() => this.closeTab(tab)}/>}
                             className={'Tab'}
                             label={tab.formatted_address}>
-
-                            {`Temperature: ${tab.weather.temp}`}
-                            <br/>
-                            {`Pressure: ${tab.weather.pressure}`}
+                            <div className={b('content')}>
+                                <br/>
+                                <br/>
+                                {`Temperature: ${tab.weather.temp} °C`}
+                                <br/>
+                                <br/>
+                                {`Pressure: ${tab.weather.pressure} Pa`}
+                            </div>
                         </Tab>
                     })
 
--- src/helpers/validators/location.js
+++
@@ -1,9 +0,0 @@
-const validate = (values) => {
-    const errors = {};
-
-    if (!values.location) {
-        errors.location = 'Required';
-    }
-    return errors;
-};
-export default validate;
